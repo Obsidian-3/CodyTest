4e87b15d774d59649e55b985df1462b9bcd882e3
Fosse <frostyfosse@users.noreply.github.com>
2022-08-24 12:48:40 -0400

    Adding initial set

---
+++ SampleExercises/SampleExercises/Program.cs
@@ -0,0 +1,14 @@
+﻿var dataSourcesDirectory = Path.Combine(Environment.CurrentDirectory, "DataSources");
+var personsFilePath = Path.Combine(dataSourcesDirectory, "Persons_20220824_00.json");
+var organizationsFilePath = Path.Combine(dataSourcesDirectory, "Organizations_20220824_00.json");
+var vehiclesFilePath = Path.Combine(dataSourcesDirectory, "Vehicles_20220824_00.json");
+var addressesFilePath = Path.Combine(dataSourcesDirectory, "Addresses_20220824_00.json");
+
+//Quick test to ensure that all files are where they should be :)
+foreach (var path in new[] { personsFilePath, organizationsFilePath, vehiclesFilePath, addressesFilePath })
+{
+    if (!File.Exists(path))
+        throw new Exception();
+}
+
+Console.WriteLine("Hello World! Welcome to Data!");
